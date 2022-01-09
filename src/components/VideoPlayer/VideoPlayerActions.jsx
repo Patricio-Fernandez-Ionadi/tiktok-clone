53694f887d822d3e07780215d8bcfcb8955fd233
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-01-09 17:54:29 -0300

    feat - add new icons

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,27 @@
 import styles from './styles.module.css'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes'
+import ShareIcon from '@mui/icons-material/Share'
 
-export default function VideoPlayerActions() {
+export default function VideoPlayerActions({
+	likes = 2041,
+	comments = 333,
+	shares = 520,
+	hearted = false,
+}) {
 	return (
 		<aside className={styles.actions}>
 			<div className={styles.action}>
 				<FavoriteIcon />
+				<strong title='like'>{likes}</strong>
 			</div>
 			<div className={styles.action}>
-				<FavoriteIcon />
+				<SpeakerNotesIcon />
+				<strong title='comments'>{comments}</strong>
 			</div>
 			<div className={styles.action}>
-				<FavoriteIcon />
+				<ShareIcon />
+				<strong title='shares'>{shares}</strong>
 			</div>
 		</aside>
 	)
